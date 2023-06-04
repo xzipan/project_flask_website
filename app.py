4b55c3c1ea8c7dce3cb6992588e147420ec66a9c
pongnara <pongnarapan@gmail.com>
2023-06-04 17:41:05 +0700

    flask-web-project-heroku

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 import uuid
-import cv2
+import tensorflow as tf
+import numpy as np
+import keras
+import random
+from PIL import Image
 from flask import Flask, flash, request, redirect, render_template, jsonify
 from flask_mysqldb import MySQL
+from keras.models import load_model
 from document_scanner import document_scanner
 
 app = Flask(__name__, static_url_path='/static')
@@ -46,46 +51,221 @@ def save_temp_file(file):
     file.save(temp_filepath)
     return temp_filepath
 
+tf.keras.models.Model
+SEED=0
+os.environ['PYTHONHASHSEED']=str(SEED)
+os.environ['TF_CUDNN_DETERMINISTIC'] = '1'  # new flag present in tf 2.0+
+random.seed(SEED)
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+from pythainlp.util import *
+#import pytesseract
+image_width = 150
+image_height = 32
+
+model_path = 'model_adam_21000_300.h5'
+weights_path = 'model_weights_adam_21000_300.h5'
+
+model = load_model(model_path)
+model.load_weights(weights_path)
+
+def num_char():
+    max_len = 35
+    characters = ['C', 'ณ', 'ร', 'ก', 'า', 'ฉ', 'ฐ', 'ี', 'ซ', 'ต', 'ญ', 'a', 'ฑ', 'ื', '7', 'v', 'ง', 'เ',
+                   '2', 'ฒ', 'อ', 'B', 'ฤ', 'ฎ', 'ท', 'ภ', ' ', '๋', 'ฏ', 'e', 'ถ', ',', 'ฃ', 'ข', '5', 'ธ',
+                     'บ', '์', 'ๆ', 'โ', 'ฯ', 'o', 'ห', '1', 'พ', '-', 'ค', '0', 'ะ', '8', 'ษ', '้', 'ป', 'c', 'd',
+                       '.', 'ไ', 'ฮ', 'น', '่', 'n', 'จ', 'ฆ', 'แ', 'ิ', '3', 'ม', 'ช', '/', '6', '็', 'ุ', 'ฅ', 'ั', '4',
+                         'A', 'ใ', '9', 'ฝ', 'ว', 'r', 'ฟ', 'ส', 'ล', 'ู', 'ฬ', 'ำ', 'ผ', 'ย', 'ด', 'ึ', 'ศ']
+    xx = collate(characters)
+    AUTOTUNE = tf.data.AUTOTUNE
+    #สับคำว่ามีตัวพญัญชนะ
+    # Mapping characters to integers.
+    char_to_num = tf.keras.layers.StringLookup(vocabulary=xx, mask_token=None)
+    # Mapping integers back to original characters.
+    num_to_char = tf.keras.layers.StringLookup(vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True)
+    
+    return num_to_char,max_len,char_to_num
+
+num_to_char,max_len,char_to_num = num_char()
+
+def numpy_to_tensor(array):
+    array = tf.convert_to_tensor(array, dtype=tf.float32)
+    array = tf.expand_dims(array,2)
+    array = distortion_free_resize(array,img_size = (image_width, image_height))
+    array = tf.cast(array, tf.float32) / 255.0
+
+    return array
+
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search.
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][:, :max_len]
+    # Iterate over the results and get back the text.
+    output_text = []
+    for res in results:
+        res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        output_text.append(res)
+
+    return output_text
+
+def distortion_free_resize(image, img_size):
+    w, h = img_size
+    image = tf.image.resize(image, size=(h, w), preserve_aspect_ratio=True)
+    # Check tha amount of padding needed to be done.
+    pad_height = h - tf.shape(image)[0]
+    pad_width = w - tf.shape(image)[1]
+    # Only necessary if you want to do same amount of padding on both sides.
+    if pad_height % 2 != 0:
+        height = pad_height // 2
+        pad_height_top = height + 1
+        pad_height_bottom = height
+    else:
+        pad_height_top = pad_height_bottom = pad_height // 2
+    if pad_width % 2 != 0:
+        width = pad_width // 2
+        pad_width_left = width + 1
+        pad_width_right = width
+    else:
+        pad_width_left = pad_width_right = pad_width // 2
+    image = tf.pad(
+        image,
+        paddings=[
+            [pad_height_top, pad_height_bottom],
+            [pad_width_left, pad_width_right],
+            [0, 0],
+        ],
+    )
+    image = tf.transpose(image, perm=[1, 0, 2])
+    image = tf.image.flip_left_right(image)
+
+    return image
+
+def crop_fields(img_output):
+    crop_field = []
+    crop_field.append(img_output[285:285+70, 900:900+140])  # call time (โทรภายใน)
+    crop_field.append(img_output[350:350+70, 750:750+270])  # date 1 (วันที่)
+    crop_field.append(img_output[612:612+70, 625:625+210])  # message (บันทึกข้อความที่)
+    crop_field.append(img_output[612:612+70, 900:900+300])  # date 2 (ลงวันที่)
+    crop_field.append(img_output[674:674+70, 600:600+600])  # for 1 (เพื่อ)
+    crop_field.append(img_output[737:737+70, 210:210+250])  # for 2 (เพื่อ-ต่อ)
+    crop_field.append(img_output[737:737+70, 590:590+355])  # on (ณ)
+    crop_field.append(img_output[737:737+70, 1070:1070+120])  # during the day 1 (ระหว่างวันที่)
+    crop_field.append(img_output[799:799+70, 210:210+445])  # during the day 2 (ระหว่างวันที่-ต่อ)
+    crop_field.append(img_output[985:985+70, 305:305+200])  # money category (หมวดเงิน)
+    crop_field.append(img_output[985:985+70, 580:580+680])  # list (รายการ)
+    crop_field.append(img_output[1049:1049+70, 285:285+175])  # money (จำนวน)
+    crop_field.append(img_output[1049:1049+70, 525:525+460])  # thai language money (จำนวนภาษาไทย)
+    crop_field.append(img_output[1410:1410+70, 875:875+230])  # name 1 (ชื่อบน)
+    crop_field.append(img_output[1468:1468+70, 865:865+260])  # position 1 (ตำแหน่งบน)
+    crop_field.append(img_output[1670:1670+70, 875:875+230])  # name 2 (ชื่อล่าง)
+    crop_field.append(img_output[1728:1728+70, 865:865+260])  # position 2 (ตำแหน่งล่าง)
+
+    return crop_field
+
 # Routes
 @app.route('/')
 def upload_form():
-    return render_template('index.html')
+    return render_template('index.html', processed_image=None)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        files = request.files.getlist('files[]')
-
-        if 'files[]' not in request.files:
+        file = request.files['file']
+        if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
 
-        for file in files:
-            if file and allowed_file(file.filename):
-                name = request.form['inputName']
-                # Save the temporary file
-                temp_filepath = save_temp_file(file)  
-                # Process the uploaded file with the document scanner
-                _, _, _, warped, _ = document_scanner(temp_filepath)
+        if file and allowed_file(file.filename):
+            # Save the temporary file
+            temp_filepath = save_temp_file(file)
+            # Process the uploaded file with the document scanner
+            warped = document_scanner(temp_filepath)
+            crop_field_images = crop_fields(warped)
+            imgto_tensor = []
+            # Convert to grayscale and array to tensor
+            for i in range(len(crop_field_images)):
+                crop_field_images[i] = Image.fromarray(crop_field_images[i])
+                crop_field_images[i] = crop_field_images[i].convert("L")
+                imgto_tensor.append(numpy_to_tensor(crop_field_images[i]))
+                # Expand dimensions of the image tensor
+                imgto_tensor[i] = tf.expand_dims(imgto_tensor[i], 0)
+
+            data_text = []
+            for i in range(len(imgto_tensor)):
+                preds = model.predict(imgto_tensor[i])
+                pred_texts = decode_batch_predictions(preds)
+                if pred_texts == "" or not pred_texts:
+                    data_text.append("-")
+                else:
+                    data_text.append(pred_texts)
+            
+            for i in range(len(data_text)):
+                print(data_text[i], i)
+
+            # Clean up the temporary file
+            os.remove(temp_filepath)
+            if warped is not None:
+                # Generate a unique filename
                 filename = str(uuid.uuid4()) + '.png'
+                # Set the output filepath
                 output_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 # Save the processed image
-                if warped is not None:
-                    cv2.imwrite(output_filepath, warped)
-                    # Clean up the temporary file
-                    os.remove(temp_filepath)
-                    #SQL
-                    cur = mysql.connection.cursor()
-                    cur.execute(
-                        'INSERT INTO flask_test (form_filename, form_name) VALUES (%s,  %s)', (filename, name))
-                    mysql.connection.commit()
-                    cur.close()
-                    flash('Files successfully uploaded.')
-                else:
-                    flash('Cannot save the image as warped is empty.')
+                processed_image = Image.fromarray(warped)
+                processed_image.save(output_filepath)
+                flash('Document scanned.')
+            else:
+                flash('Cannot scan the image as warped is empty.')
 
+            return render_template('index.html', processed_image=filename, data_text=data_text)
+
+    return render_template('index.html')
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    if request.method == 'POST':
+        # Get the form data
+        form_filename = request.form.get('processed_image')
+        form_name = request.form.get('inputName')
+
+        f_call = request.form.get('callTime')
+        f_date = request.form.get('callDate')
+        f_msg = request.form.get('message')
+        f_msgdate = request.form.get('dateSign')
+        f_for1 = request.form.get('for')
+        f_for2 = request.form.get('forContinue')
+        f_where = request.form.get('on')
+        f_between1 = request.form.get('duringTheDay')
+        f_between2 = request.form.get('duringTheDayContinue')
+        f_category = request.form.get('moneyCategory')
+        f_list = request.form.get('list')
+        f_money = request.form.get('money')
+        f_moneyth = request.form.get('thaiMoney')
+        f_signup = request.form.get('nameUpper')
+        f_signupposition = request.form.get('positionUpper')
+        f_signdown = request.form.get('nameLower')
+        f_signdownposition = request.form.get('positionLower')
+
+        # Insert data into the "flask_test" table
+        cur = mysql.connection.cursor()
+        insert_query = "INSERT INTO flask_test (form_filename, form_name) VALUES (%s, %s)"
+        values = (form_filename, form_name)
+        cur.execute(insert_query, values)
+        form_id = cur.lastrowid  # Retrieve the generated form_id
+        mysql.connection.commit()
+        cur.close()
+
+        # Insert data into the "forms" table
+        cur = mysql.connection.cursor()
+        insert_query = "INSERT INTO forms (f_call, f_date, f_msg, f_msgdate, f_for1, f_for2, f_where, f_between1, f_between2, f_category, f_list, f_money, f_moneyth, f_signup, f_signupposition, f_signdown, f_signdownposition, f_img) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (f_call, f_date, f_msg, f_msgdate, f_for1, f_for2, f_where, f_between1, f_between2, f_category, f_list, f_money, f_moneyth, f_signup, f_signupposition, f_signdown, f_signdownposition, form_id)
+        cur.execute(insert_query, values)
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Data saved successfully.')
         return redirect('/')
-
+    
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
@@ -122,44 +302,75 @@ def delete_records():
 
     return redirect('/search')
 
-@app.route('/get_admin_password')
-def get_admin_password():
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT admin_password FROM admin")
-    result = cursor.fetchone()
-    if result:
-        return jsonify({'password': result[0]})
-    else:
-        return jsonify({'password': None})
+@app.route('/check_admin_password')
+def check_admin_password():
+    user = request.args.get('user')
+    password = request.args.get('password')
 
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT admin_name, admin_password FROM admin WHERE admin_name = %s AND admin_password = %s", (user, password))
+    result = cur.fetchone()
+
+    if result:
+        return jsonify(valid=True)
+    else:
+        return jsonify(valid=False)
+    
 @app.route('/results')
 def result():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM flask_test')
     flask_all = cur.fetchall()
-    return render_template('result.html', flask_all = flask_all)
+    return render_template('result.html', flask_all=flask_all)
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
     if request.method == 'POST':
         query = request.form['id']
         cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT * FROM flask_test WHERE form_id LIKE %s", ('%' + query + '%',))
+        cur.execute("""
+            SELECT flask_test.*, forms.f_img, forms.f_update, forms.f_call, forms.f_date, forms.f_msg, forms.f_msgdate,
+            forms.f_for1, forms.f_for2, forms.f_where, forms.f_between1, forms.f_between2, forms.f_category,
+            forms.f_list, forms.f_money, forms.f_moneyth, forms.f_signup, forms.f_signupposition,
+            forms.f_signdown, forms.f_signdownposition
+            FROM flask_test
+            LEFT JOIN forms ON flask_test.form_id = forms.f_img
+            WHERE flask_test.form_id LIKE %s
+        """, ('%' + query + '%',))
         data = cur.fetchall()
-        return render_template('edit.html', data = data)
+        return render_template('edit.html', data=data)
 
     return render_template('edit.html')
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def update(id):
     if request.method == 'POST':
-        name = request.form['name']
+        inputName = request.form['inputName']
+        callDate = request.form['callDate']
+        dateSign = request.form['dateSign']
+        forContinue = request.form['forContinue']
+        duringTheDay = request.form['duringTheDay']
+        moneyCategory = request.form['moneyCategory']
+        money = request.form['money']
+        nameUpper = request.form['nameUpper']
+        nameLower = request.form['nameLower']
+        callTime = request.form['callTime']
+        message = request.form['message']
+        forField = request.form['for']
+        onField = request.form['on']
+        duringTheDayContinue = request.form['duringTheDayContinue']
+        listField = request.form['list']
+        thaiMoney = request.form['thaiMoney']
+        positionUpper = request.form['positionUpper']
+        positionLower = request.form['positionLower']
+        
         cur = mysql.connection.cursor()
-        cur.execute("UPDATE flask_test SET form_name = %s WHERE form_id = %s", (name, id,))
+        cur.execute("UPDATE flask_test SET form_name = %s WHERE form_id = %s", (inputName, id,))
+        cur.execute("UPDATE forms SET f_call = %s, f_date = %s, f_msg = %s, f_msgdate = %s, f_for1 = %s, f_for2 = %s, f_where = %s, f_between1 = %s, f_between2 = %s, f_category = %s, f_list = %s, f_money = %s, f_moneyth = %s, f_signup = %s, f_signupposition = %s, f_signdown = %s, f_signdownposition = %s WHERE f_img = %s", 
+                    (callTime, callDate, message, dateSign, forField, forContinue, onField, duringTheDay, duringTheDayContinue, moneyCategory, listField, money, thaiMoney, nameUpper, positionUpper, nameLower, positionLower, id,))
         mysql.connection.commit()
         cur.close()
         return render_template('edit.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- document_scanner.py
+++ document_scanner.py
@@ -1,5 +1,5 @@
-import numpy as np
 import cv2
+import numpy as np
 import imutils
 
 def preprocess_image(image_path, height=500):
@@ -7,16 +7,19 @@ def preprocess_image(image_path, height=500):
     ratio = image.shape[0] / float(height)
     orig = image.copy()
     image = imutils.resize(image, height=height)
+
     return image, orig, ratio
 
 def detect_edges(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
     return cv2.Canny(gray, 75, 200)
 
 def find_contours(edged):
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+
     return sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 
 def find_screen_contour(contours):
@@ -28,11 +31,9 @@ def find_screen_contour(contours):
 
 def order_points(pts):
     rect = np.zeros((4, 2), dtype="float32")
-
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
-
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
@@ -41,24 +42,13 @@ def order_points(pts):
 
 def four_point_transform(image, pts):
     rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-
     dst = np.array([
         [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
-
+        [1356, 0],
+        [1356, 1919],
+        [0, 1919]], dtype="float32")
     M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    warped = cv2.warpPerspective(image, M, (1357, 1920))
 
     return warped
 
@@ -67,21 +57,12 @@ def document_scanner(image_path):
     edged = detect_edges(image)
     cnts = find_contours(edged)
     screenCnt = find_screen_contour(cnts)
-    
     if screenCnt is not None:
+        print("Document warped.")
         warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+        return warped
+
     else:
         print("No contour with 4 points found.")
         warped = None
-
-    return image, orig, edged, warped, screenCnt
-
-# def display_images(image, orig, edged, warped, screenCnt):
-#     cv2.imshow("Edged", edged)
-#     cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-#     cv2.imshow("Outline", image)
-#     cv2.imshow("Original", imutils.resize(orig, height=500))
-#     cv2.imshow("Scanned", imutils.resize(warped, height=500))
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
+        return warped
